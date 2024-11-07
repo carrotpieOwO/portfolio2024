59e43fdb6166f06436931ff0a3cd3050af4b236e
carrotpieOwO <xxhayoxx@naver.com>
2024-11-07 15:24:28 +0900

    feat: about 페이지 내용 추가

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,28 +1,47 @@
 'use client';
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useEffect, useRef } from "react";
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
 import { TypeAnimation } from "react-type-animation";
-
+import { aboutInfo } from "../utill/aboutInfo";
 
 export default function About() {
-  const ref = useRef<HTMLDivElement>(null);
+  const [character, setCharacter] = useState('default');
+  const titleRef = useRef<HTMLDivElement>(null);
+  const typingRef = useRef<HTMLDivElement>(null);
+
   gsap.registerPlugin(ScrollTrigger);
   useEffect(() => {
-    const section = ref.current;
+    const title = titleRef.current;
+    const typing = typingRef.current;
 
-    gsap.fromTo(section,
-      { opacity: 0, y: 500 },
+    gsap.fromTo(title,
+      { y: 100 },
+      {
+        y: 0,
+        scrollTrigger: {
+          trigger: title,
+          start: "top bottom", // 요소의 top이 viewport의 bottom에 도달했을 때 시작
+          end: "bottom top", // 요소의 bottom이 viewport의 top에 도달했을 때 끝
+          toggleActions: "play none none reverse", // 진입 시 재생, 퇴장 시 역재생
+          scrub: true, // 스크롤과 애니메이션을 연동하려면 이 옵션을 사용
+        }
+      }
+    );
+
+    gsap.fromTo(typing,
+      { opacity: 0, y: 100 },
       {
         opacity: 1,
         y: 0,
         fontSize: '2rem',
-        duration: 1,
+        duration: 2,
         scrollTrigger: {
-          trigger: section,
+          trigger: typing,
           start: "top bottom", // 요소의 top이 viewport의 bottom에 도달했을 때 시작
           end: "bottom top", // 요소의 bottom이 viewport의 top에 도달했을 때 끝
-          toggleActions: "play none none reverse", // 진입 시 재생, 퇴장 시 역재생
+          toggleActions: "play none none", // 진입 시 재생, 퇴장 시 역재생
           //scrub: true, // 스크롤과 애니메이션을 연동하려면 이 옵션을 사용
         }
       }
@@ -44,12 +63,16 @@ export default function About() {
   }, []);
 
   return (
-    <section ref={ref} style={{ height: '100vh'}}>
-      <div className="letterVariants">
-          <div>
+    <section className="relative h-[100vh] w-full">
+      <div ref={titleRef} className="w-full absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-4xl xl:text-7xl font-bold">
+        FRONT-END DEVELOPER
+      </div>
+      <div className="about-container w-full h-full absolute top-10 left-1/2 transform -translate-x-1/2 bg-white py-10">
+        <div ref={typingRef}>
+          <div className="flex justify-center items-center">
               안녕하세요!&nbsp;
-              <div className="shakeVariants">
-                  👋🏻
+              <div className="shake">
+                👋🏻
               </div> 
           </div>
           <span>저는&nbsp;</span>
@@ -73,7 +96,55 @@ export default function About() {
               ]}
               repeat={Infinity}
           />
-          <span>&nbsp;최하영입니다. :)</span>{' '}
+          <span>&nbsp;최하영입니다 :)</span>
+        </div>
+        <Image 
+          src={`/about-${character}.png`} alt="profile" width={300} height={300} 
+          className={`mx-auto transition-opacity duration-300 ease-in-out`}
+        />
+        <div id="skill" className="flex flex-wrap flex-col justify-center items-center bg-black w-fit mx-auto py-2 px-10 rounded-md gap-3">
+          <div className="flex flex-wrap gap-3 text-white">
+            <span>Language</span>
+            <img src="https://img.shields.io/badge/Javascript-F7DF1E?style=flat&amp;logo=Javascript&amp;logoColor=black" alt="Javascript" />
+            <img src="https://img.shields.io/badge/TypeScript-3178C6?style=flat&amp;logo=TypeScript&amp;logoColor=white" alt="TypeScript" />
+            <img src="https://img.shields.io/badge/jquery-0769AD?style=flat&amp;logo=jquery&amp;logoColor=white" alt="Chart.js" />
+          </div>  
+          <div className="flex flex-wrap gap-3 text-white">
+            <span>Front-End</span>
+            <img src="https://img.shields.io/badge/Next.js-000000?style=flat&amp;logo=next.js&amp;logoColor=white" alt="Chart.js" />
+            <img src="https://img.shields.io/badge/React-61DAFB?style=flat&amp;logo=React&amp;logoColor=black" alt="React" />
+            <img src="https://img.shields.io/badge/React Query-FF4154?style=flat&amp;logo=ReactQuery&amp;logoColor=white" alt="Chart.js" />
+            <img src="https://img.shields.io/badge/angularJS-DD0031?style=flat&amp;logo=angular&amp;logoColor=white" alt="angularJS" />
+          </div>
+          <div className="flex flex-wrap gap-3 text-white">
+            <span>Library</span>
+            <img src="https://img.shields.io/badge/Chart.js-FF6384?style=flat&amp;logo=Chart.js&amp;logoColor=white" alt="Chart.js" />
+            <img src="https://img.shields.io/badge/recharts-f67377?style=flat&amp;logo=recharts.js&amp;logoColor=white" alt="Chart.js" />
+            <img src="https://img.shields.io/badge/D3-F9A03C?style=flat&amp;logo=d3.js&amp;logoColor=white" alt="Chart.js" />
+          </div>
+          <div className="flex flex-wrap gap-3 text-white">
+            <span>Styling</span>
+            <img src="https://img.shields.io/badge/styled component-DB7093?style=flat&amp;logo=styledcomponents&amp;logoColor=white" alt="Chart.js" />
+            <img src="https://img.shields.io/badge/tailwindcss-06B6D4?style=flat&amp;logo=tailwindcss&amp;logoColor=white" alt="Chart.js" />
+            <img src="https://img.shields.io/badge/emotion-f67377?style=flat&amp;logo=emotion&amp;logoColor=white" alt="Chart.js" />
+            <img src="https://img.shields.io/badge/MUI-007FFF?style=flat&amp;logo=mui&amp;logoColor=white" alt="Chart.js" />
+            <img src="https://img.shields.io/badge/Ant Design-0170FE?style=flat&amp;logo=antdesign&amp;logoColor=white" alt="Chart.js" />
+          </div>
+        </div>
+        <div className="w-fit mx-auto flex flex-wrap gap-3 mt-3" onMouseLeave={() => setCharacter('default')}>
+          {
+            aboutInfo.map((info) => (
+              <span 
+                key={info.key} 
+                className="bg-pink-500 text-white font-bold rounded-full px-4 py-2 cursor-pointer" 
+                onMouseEnter={() => setCharacter(info.key)}
+                onClick={() => window.open(info.url, '_blank')}
+              >
+                #{info.key}
+              </span>
+            ))
+          }
+        </div>
       </div>
     </section>
   );
---
+++ app/utill/aboutInfo.js
@@ -0,0 +1,22 @@
+export const aboutInfo = [
+    {
+        key: 'resume',
+        message: '저에 대해 더 궁금하시다면?',
+        url: 'https://drive.google.com/file/d/1000000000000000000000000000000000000000/view?usp=sharing'
+    },
+    {
+        key: 'github',
+        message: '제 코드와 프로젝트를 확인해보세요!',
+        url: 'https://github.com/carrotpieOwO/'
+    },
+    {
+        key: 'blog',
+        message: '제 생각과 경험을 공유합니다.',
+        url: 'https://ha0-dev.tistory.com/'
+    },
+    {
+        key: 'email',
+        message: '언제든 연락 주세요!',  
+        url: 'mailto:ha0@kakao.com'
+    }
+]
